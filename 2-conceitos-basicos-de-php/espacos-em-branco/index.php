42591b3acdc42f5819874db603b33946f4cd3165
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-10-31 13:11:26 -0300

    2: Espaços em branco

---
+++ 2-conceitos-basicos-de-php/espacos-em-branco/index.php
@@ -0,0 +1,12 @@
+<?php
+echo                            "testando espaço em branco <br />";
+echo "testando espaço <br />"                            ;
+echo 
+"quebra de linha <br />"
+;
+echo "quebra de linha <br />"
+;
+echo
+"quebra de 
+linha <br />"
+;
