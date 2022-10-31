706718495a783292aca306abed53c803cf47a873
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-10-31 13:00:55 -0300

    2: Sensibilidade de case

---
+++ 2-conceitos-basicos-de-php/sensibilidade-de-case/index.php
@@ -0,0 +1,13 @@
+<?php
+// Não é case sensitive
+echo "teste <br />";
+eChO "testando 2 <br />";
+ECHO "testando 3 <br />";
+
+// É case sensitive
+$nome = "Leonardo";
+$NOME = "Teste";
+
+echo $nome;
+echo "<br />";
+echo $NOME;
