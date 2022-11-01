d6c7aebd769fa0aee733dcc91cb2d7a23b9f0af3
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-10-31 22:02:04 -0300

    3: Arrays

---
+++ 3-tipos-de-dados/arrays/index.php
@@ -0,0 +1,11 @@
+<?php
+$arr = ['nome' => 'Shark', 'patas' => 4, 'cor' => 'Marrom'];
+echo $arr['nome'];
+echo '<br />';
+print_r($arr);
+echo '<br />';
+echo $arr['patas'];
+echo '<br />';
+
+$arrAssoc = ['chave' => 'valor', 'bool' => true];
+print_r($arrAssoc);
