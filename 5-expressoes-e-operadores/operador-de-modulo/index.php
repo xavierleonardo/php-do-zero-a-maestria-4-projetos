7a100862e4c140087c131057cb69b88ee0104d30
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-04 12:57:39 -0300

    5: Operador de módulo

---
+++ 5-expressoes-e-operadores/operador-de-modulo/index.php
@@ -0,0 +1,13 @@
+<?php
+echo 12 % 2 . ' módulo<br />';
+echo 12 / 2 . ' divisão<br />';
+
+echo 13 % 2 . '<br />';
+echo 13 % 5 . '<br />';
+
+$a = 20;
+$b = 4;
+$c = 12;
+
+echo $a % $b . '<br />';
+echo $a % $c . '<br />';
