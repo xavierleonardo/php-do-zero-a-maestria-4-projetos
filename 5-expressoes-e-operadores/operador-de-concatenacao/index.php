460b64feeaee4a7460abfc76e5f9f9452a1eff1b
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-04 13:09:47 -0300

    5: Operador de concatenação

---
+++ 5-expressoes-e-operadores/operador-de-concatenacao/index.php
@@ -0,0 +1,12 @@
+<?php
+echo 'testando a concatenação<br />';
+echo 'testando ' . 'a ' . 'concatenação<br />';
+
+$t = 'testando';
+$c = 'concatenação';
+echo $t . ' a ' . $c . '<br />';
+
+$marca = 'Ferrari';
+$motor = '3.0';
+$vel_max = 200;
+echo 'O carro da ' . $marca . ' tem um motor ' . $motor . ' e chega uma velocidade de ' . $vel_max . 'km/h<br />';
