a69d76e6647bdd919b010f856f669b9608f90ddc
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-03 21:05:34 -0300

    4: Declarando variáveis

---
+++ 4-variaveis/declarando-variaveis/index.php
@@ -0,0 +1,15 @@
+<?php
+$teste = "Algum texto";
+echo $teste;
+echo '<br />';
+
+$num = 10;
+echo $num;
+echo '<br />';
+
+$_nome = 'Leonardo';
+echo $_nome;
+echo '<br/>';
+
+$velocidadeMaxima = 100;
+$velocidade_minima = 100;
