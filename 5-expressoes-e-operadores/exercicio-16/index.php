a07852cb4c394793e4d017c9c1408d54b2054985
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-04 13:47:03 -0300

    5: Exercício 16

---
+++ 5-expressoes-e-operadores/exercicio-16/index.php
@@ -0,0 +1,19 @@
+<?php
+$a = 5;
+$b = 3;
+
+if($a == $b) {
+  echo 'Igual<br />';
+}
+
+if($a != $b) {
+  echo 'Diferente<br />';
+}
+
+if($a === $b) {
+  echo 'Idêntico<br />';
+}
+
+if($a !== $b) {
+  echo 'Não idêntico<br />';
+}
