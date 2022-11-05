eeab1173da0c0105fefcf77189a848507da28006
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-05 11:15:19 -0300

    6: Estrutura If

---
+++ 6-estruturas-de-controle/estrutura-if/index.php
@@ -0,0 +1,23 @@
+<?php
+// condição verdadeira
+if(5 > 2) {
+  echo 'Deu certo! Entrou no if<br />';
+}
+
+// condição false
+if(2 >= 5) {
+  echo 'Não vai entrar no if, porque deu false!<br />';
+}
+
+// utilizar op. lógico
+if(10 === 10 && 9 > 3) {
+  echo 'Deu certo! Entrou no if 2<br />';
+}
+
+// variáveis
+$a = 10;
+$b = 5;
+$c = 'Deu certo, entrou no if 3<br />';
+if($a >= $b) {
+  echo $c;
+}
