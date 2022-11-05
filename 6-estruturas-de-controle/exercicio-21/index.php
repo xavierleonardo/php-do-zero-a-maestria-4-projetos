adde5cc452c2dc0dd7d20f7af080f32dd5d4845d
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-05 11:19:42 -0300

    6: Exercício 21

---
+++ 6-estruturas-de-controle/exercicio-21/index.php
@@ -0,0 +1,19 @@
+<?php
+// condição verdadeira
+$a = 5 > 2;
+$b = 'Matheus' != 'Pedro';
+$c = 12 >= 11;
+
+echo 'Verdadeiros: ';
+
+if($a) {
+  echo 'A, ';
+}
+
+if($b) {
+  echo 'B e ';
+}
+
+if($c) {
+  echo 'C';
+}
