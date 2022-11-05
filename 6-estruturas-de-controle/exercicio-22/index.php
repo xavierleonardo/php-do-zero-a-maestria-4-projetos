d92117d3d26aac98e8fb233cdd2482afdd4229b4
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-05 11:23:29 -0300

    6: Exercício 22

---
+++ 6-estruturas-de-controle/exercicio-22/index.php
@@ -0,0 +1,17 @@
+<?php
+// condição verdadeira
+$a = 15;
+$b = 18;
+$c = 34;
+
+if($a >= 18) {
+  echo 'A é maior de idade<br />';
+}
+
+if($b >= 18) {
+  echo 'B é maior de idade<br />';
+}
+
+if($c >= 18) {
+  echo 'C é maior de idade<br />';
+}
