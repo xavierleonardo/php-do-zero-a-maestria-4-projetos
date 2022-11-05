49d43582350c8b33e00c9ecb4b490cca0b0620c9
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-05 12:07:32 -0300

    6: Estrutura Switch

---
+++ 6-estruturas-de-controle/estrutura-switch/index.php
@@ -0,0 +1,24 @@
+<?php
+$x = 0;
+switch($x) {
+  case 0:
+    echo 'X é igual a 0<br />';
+    break;
+  case 1:
+    echo 'X é igual a 1<br />';
+    break;
+  default:
+    echo 'X não é igual a nenhum dos valores mencionados<br />';
+}
+
+$y = 'Leonardo';
+switch($x) {
+  case 'Leonardo':
+    echo 'O nome é Leonardo<br />';
+    break;
+  case 'João':
+    echo 'O nome é João<br />';
+    break;
+  default:
+    echo 'O nome não foi encontrado<br />';
+}
