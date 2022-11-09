b785fda965485685447dfc2cff939aa7eceeab03
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-09 13:27:40 -0300

    8: Exibição de conteúdo

---
+++ 8-inclusao-de-codigo/exibicao-de-conteudo/index.php
@@ -0,0 +1,16 @@
+<?php
+  $nome = 'Leonardo';
+  $sobrenome = 'Xavier';
+?>
+
+<form action="">
+  <div>
+    <input type="text" value="<?= $nome; ?>" />
+  </div>
+  <div>
+    <input type="text" value="<?= $sobrenome; ?>" />
+  </div>
+  <div>
+    <input type="submit" value="Enviar" />
+  </div>
+</form>
