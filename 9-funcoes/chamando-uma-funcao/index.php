f7890779b65b2725256e07030a8567a4eda35ff4
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-10 15:35:01 -0300

    9: Chamando uma função

---
+++ 9-funcoes/chamando-uma-funcao/index.php
@@ -0,0 +1,15 @@
+<?php
+// definindo função
+function soma() {
+  $a = 5;
+  $b = 4;
+
+  echo $a + $b . '<br />';
+}
+
+// chamando/invocando função
+soma();
+soma();
+
+// função do PHP
+echo strtoupper('testando função');
