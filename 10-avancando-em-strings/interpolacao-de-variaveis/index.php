0225874ec2480509c913279033e52ef31d437b9b
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-12 14:38:34 -0300

    10: Interpolação de variáveis

---
+++ 10-avancando-em-strings/interpolacao-de-variaveis/index.php
@@ -0,0 +1,7 @@
+<?php
+$nome = 'Leonardo';
+$idade = 34;
+$profissao = 'Programador';
+
+echo "Eu sou o $nome e tenho $idade anos, e atuo como $profissao<br />";
+echo "Eu sou o {$nome} e tenho {$idade} anos, e atuo como {$profissao}<br />";
