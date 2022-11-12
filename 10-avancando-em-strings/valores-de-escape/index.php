8dbfca65b51381df9f819eb1c81048840dee4a4f
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-12 14:44:05 -0300

    10: Valores de escape

---
+++ 10-avancando-em-strings/valores-de-escape/index.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-type: text/plain');
+
+// pular linha
+echo "Isso aqui vai ficar na primeira linha\nE isso na segunda linha\n";
+
+// Tab
+echo "Testando o tab \t aqui\n";
+
+// Barra invertida
+echo "Barra invertida \\\n";
+
+// Imprimindo o dólar
+echo "Barra invertida \$teste";
