1d5201f46b6f33c05cde52956300d23ee1985356
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-13 13:37:12 -0300

    11: Exercício 47

---
+++ 11-avancando-em-arrays/exercicio-47/index.php
@@ -0,0 +1,10 @@
+<?php
+$carro = ['jaguar', 3.0, 'azul', 18, true, true];
+list($modelo, $motor, $cor, $idade, $tetoSolar, $automatico) = $carro;
+
+echo "$modelo <br />";
+echo "$motor <br />";
+echo "$cor <br />";
+echo "$idade <br />";
+echo "$tetoSolar <br />";
+echo "$automatico <br />";
