ffd9713ef4777f5bcf247ee9837c29c67effd6b5
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-13 13:17:14 -0300

    11: Número de elementos

---
+++ 11-avancando-em-arrays/numero-de-elementos/index.php
@@ -0,0 +1,9 @@
+<?php
+$arr = [1, 2, 3];
+echo count($arr) . '<br />';
+
+$arr2 = range(1, 10);
+echo count($arr2) . '<br />';
+
+$arr3 = ['nome'=>'Leonardo', 'idade'=>34, 'profissao'=>'Programador'];
+echo count($arr3) . '<br />';
