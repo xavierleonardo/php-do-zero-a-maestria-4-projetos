f1dcc5f66b748250d616d0845d23381a02cee7ce
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-14 23:06:41 -0300

    12: Constructor

---
+++ 12-introducao-a-orientacao-de-objetos/constructor/index.php
@@ -0,0 +1,18 @@
+<?php
+class Car {
+  public $portas;
+  public $cor;
+  public $marca;
+
+  function __construct($portas, $cor, $marca) {
+    $this->portas = $portas;
+    $this->cor = $cor;
+    $this->marca = $marca;
+  }
+}
+
+$ferrari = new Car(4, 'Vermelha', 'Ferrari');
+echo "O carro é da marca $ferrari->marca e tem a cor $ferrari->cor<br />";
+
+$bmw = new Car(4, 'Verde', 'bmw');
+echo "O carro é da marca $bmw->marca e tem a cor $bmw->cor<br />";
