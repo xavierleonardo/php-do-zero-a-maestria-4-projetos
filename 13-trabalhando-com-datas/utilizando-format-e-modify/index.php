d29d139d6c318111285ee855d9a43a4293c956cd
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-15 12:28:35 -0300

    13: Utilizando format e modify

---
+++ 13-trabalhando-com-datas/utilizando-format-e-modify/index.php
@@ -0,0 +1,14 @@
+<?php
+$data = new DateTime();
+echo $data->format('d/m/y') . '<br />';
+echo $data->format('D - M - Y') . '<br />';
+echo $data->format('l . F . Y') . '<br />';
+
+$data->modify('+5 days');
+echo $data->format('d/m/y') . '<br />';
+
+$data->modify('+2 months');
+echo $data->format('d/m/y') . '<br />';
+
+$data->modify('-3 years');
+echo $data->format('d/m/y') . '<br />';
