a368cab7f43f7a42fd1c4e81649f0ab79b41b912
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-17 20:39:59 -0300

    15: Setup inicial do projeto

---
+++ 15-projeto-1_blog/index.php
@@ -0,0 +1,25 @@
+<?php
+include_once('helpers/url.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blog Codar</title>
+  <!-- ESTILOS DO PROJETO -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+  <!-- GOOGLE FONTS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <h1>Olá Mundo!</h1>
+</body>
+
+</html>
