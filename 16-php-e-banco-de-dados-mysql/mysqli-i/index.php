1d5ac3900ec1a5668b22f29e1a2205cffca907ec
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-11-21 19:05:35 -0300

    16: mysqli - I

---
+++ 16-php-e-banco-de-dados-mysql/mysqli-i/index.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'cursophp';
+$conn = new mysqli($host, $user, $pass, $db);
+
+$sql = 'SELECT * FROM itens';
+$result = $conn->query($sql);
+
+print_r($result);
+$conn->close();
